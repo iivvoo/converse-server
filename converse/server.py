ac6be9f1276378b6bc9240bf4fca343f4c276f7f
Ivo van der Wijk <github@in.m3r.nl>
2016-04-21 17:30:55 +0200

    Move webserver to its own module

--- converse/server.py
+++ converse/server.py
@@ -9,15 +9,13 @@ from urllib.parse import urlparse, parse_qsl
 import asyncio
 import websockets
 
-import aiohttp
-import aiohttp.server
-
 from .ircbackend import IRCBackend
 from .account import Account
 
 from .randombackend import RandomBackend
 from .accountmanager import manager
 from .setupwizard import initial_setup
+from .webserver import ConverseWebServer
 
 log = logging.getLogger('bottom')
 log.setLevel(logging.DEBUG)
@@ -30,33 +28,6 @@ wslog.addHandler(logging.StreamHandler())
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-class ConverseWebServer(aiohttp.server.ServerHttpProtocol):
-    async def handle_request(self, message, payload):
-        # /avatar/token/<acctid>/<backendid>/<userid>
-        parts = message.path.split('/')
-        status_code = 404
-        ct = "text/html"
-        msg = b"<h1>Not found</h1>"
-
-        if len(parts) == 6:  # ('', 'avatar', 'token', id, id, id)
-            _, ns, token, acctid, backendid, userid = parts
-            if ns == 'avatar':
-                account = manager.get_by_id(int(acctid))
-                be = account.get_backend_by_id(int(backendid))
-                user = be.get_user_by_id(int(userid))
-                msg = user.get_avatar()
-                status_code = 200
-                ct = "image/png"
-
-        response = aiohttp.Response(
-            self.writer, status_code, http_version=message.version
-        )
-        response.add_header('Content-Type', ct)
-        response.add_header('Content-Length', str(len(msg)))
-        response.send_headers()
-        response.write(msg)
-        await response.write_eof()
 
 async def server(websocket, path):  # noqa
 
---
+++ converse/webserver.py
@@ -0,0 +1,30 @@
+import aiohttp
+import aiohttp.server
+
+
+class ConverseWebServer(aiohttp.server.ServerHttpProtocol):
+    async def handle_request(self, message, payload):
+        # /avatar/token/<acctid>/<backendid>/<userid>
+        parts = message.path.split('/')
+        status_code = 404
+        ct = "text/html"
+        msg = b"<h1>Not found</h1>"
+
+        if len(parts) == 6:  # ('', 'avatar', 'token', id, id, id)
+            _, ns, token, acctid, backendid, userid = parts
+            if ns == 'avatar':
+                account = manager.get_by_id(int(acctid))
+                be = account.get_backend_by_id(int(backendid))
+                user = be.get_user_by_id(int(userid))
+                msg = user.get_avatar()
+                status_code = 200
+                ct = "image/png"
+
+        response = aiohttp.Response(
+            self.writer, status_code, http_version=message.version
+        )
+        response.add_header('Content-Type', ct)
+        response.add_header('Content-Length', str(len(msg)))
+        response.send_headers()
+        response.write(msg)
+        await response.write_eof()
